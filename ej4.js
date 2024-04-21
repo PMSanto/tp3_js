bea7c58ae4b5d571b89dbf8f54acf99a32c1e39e
Pau <pmendezsantochi@gmail.com>
2024-04-21 18:51:22 -0300

    correcion de error

--- ej4.js
+++ ej4.js
@@ -1,7 +1,7 @@
 /* 4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
  */
 
-function Paridad(numero) {
+function paridad(numero) {
   if (numero % 2 === 0) {
     console.log("Es par");
   } else {
@@ -9,6 +9,6 @@ function Paridad(numero) {
   }
 }
 
-let numero = 25;
-let resultado = Paridad(numero);
+let numero = 20;
+let resultado = paridad(numero);
 console.log(resultado);
